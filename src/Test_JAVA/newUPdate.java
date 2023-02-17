a3ee34ffc449a12dc874e0128f52aee9fd54e813
Naniram-code <Nanirambhujel@gnail.coim>
2023-02-16 19:54:39 -0500

    newUpdate java file

---
+++ src/Test_JAVA/newUPdate.java
@@ -0,0 +1,9 @@
+package Test_JAVA;
+
+public class newUPdate {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(" hellow java code ");
+System.out.println("hellow java ");
+
+}}
