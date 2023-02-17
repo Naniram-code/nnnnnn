12cf8096ef640f50525e5645642aaa11d6eb1992
Naniram-code <Nanirambhujel@gnail.coim>
2023-02-16 19:44:05 -0500

    first push

---
+++ src/Test_JAVA/code_JAVA.java
@@ -0,0 +1,45 @@
+package Test_JAVA;
+
+public class code_JAVA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(" hellow java code ");
+System.out.println("hellow java ");
+
+
+
+// println -> print in a line
+System.out.println("Hello Java. First code. Having fun.");
+System.out.println("Java is not boring.");
+System.out.println();
+System.out.println("Automation is better than manual.");
+// print - print
+System.out.print("Java Selenium for automation");
+System.out.print("Automation is faster than manual.");
+System.out.println("Manual test case writing.");
+System.out.print("Automation");
+System.out.println();
+// error message printing
+System.err.print("Invalid password.");
+// shortcut for System.out.println(); -> sysout+ctrl+space bar
+System.out.println("Enter username: ");
+System.out.println("Enter password: ");
+System.out.println("Name"+"Email address");
+// insert some tab spaces between words -> a b c
+System.out.println("Name"+"\t"+"Email address"+"\t"+"Phone number");
+System.out.println("Name"+"\t\t"+"Email address"+"\t\t"+"Phone number");
+System.out.println("David"+"\t\t"+"David@gmail.com"+"\t\t"+"123456789");
+// break statement down to different line
+System.out.println("Hello Java. "+"\n"+"First code. "+"\n"+"Having fun. "+"\n"+"Java is not boring. "+"\n"+"Automation is better than manual."
++ " "+"\n"+"Java Selenium for automation. "+"\n"+"Automation is faster than manual.");
+
+}
+// Assignment: 1. Practice all printing statements
+// 2. Copy some news paragraph -> break every sentence into multiple line
+// 3. Create header for a table and values inside the table
+
+
+	}
+
+
